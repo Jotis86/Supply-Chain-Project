055be4f1cd847aec126d2baedc82249bb8dfb28b
Jotis <juaneloyduranbon@hotmail.com>
2025-04-22 18:05:18 +0200

    refactor: Update GitHub link and add spacing for improved layout clarity

--- app/app.py
+++ app/app.py
@@ -1021,7 +1021,7 @@ def main():
         
         # GitHub button
         st.markdown("""
-        <a href="https://github.com/jotis/" class="github-button">
+        <a href="https://github.com/Jotis86/Supply-Chain-Project" class="github-button">
             <svg height="24" width="24" viewBox="0 0 16 16" version="1.1">
                 <path fill="white" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
             </svg>
@@ -1082,6 +1082,9 @@ def main():
         """, unsafe_allow_html=True)
         
         st.markdown('</div>', unsafe_allow_html=True)
+
+        # Add space
+        st.markdown("<br>", unsafe_allow_html=True)
         
         # Technologies used
         #st.markdown('<div class="card">', unsafe_allow_html=True)
@@ -1121,12 +1124,6 @@ def main():
         
         st.markdown('</div>', unsafe_allow_html=True)
         
-        # Contact information or additional resources
-        st.markdown("""
-        <div style="text-align: center; margin-top: 30px; color: #7f8c8d; font-size: 0.9rem;">
-            Developed by Jotis | For questions or suggestions, please reach out via GitHub
-        </div>
-        """, unsafe_allow_html=True)
 
     elif selection == "Dashboard":
         # Display key metrics
